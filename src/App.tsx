5f7458cd691268bf62979af0106e80ac7c5975c5
Thanh Le <letrithanh@outlook.com>
2024-07-26 18:53:05 +0700

    intergate with main app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import PageSelectorWidget from "./widgets/page-selector/page-selector.widget";
+
+function App() {
+
+    return (
+        <div className="app">
+            <PageSelectorWidget />
+        </div>
+    );
+}
+
+export default App;
